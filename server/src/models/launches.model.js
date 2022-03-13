958b619135c0c9c87942ddd7b512c2d064e29c91
Bartek <submission93@gmail.com>
2022-03-13 19:43:00 +0100

    nasa-project sunday

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,38 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+  flightNumber: 100,
+  mission: "Destory Tank",
+  rocket: "Javelin",
+  launchDate: new Date("December 2, 2049"),
+  destination: "Kiev",
+  customer: ["ZTM", "UAF"],
+  upcoming: true,
+  success: true,
+};
+
+launches.set(launch.flightNumber, launch);
+
+function getAllLaunches() {
+  return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+  latestFlightNumber++;
+  launches.set(
+    latestFlightNumber,
+    Object.assign(launch, {
+      success: true,
+      upcoming: true,
+      customers: ["ztm", "NASA"],
+      flightNumber: latestFlightNumber,
+    })
+  );
+}
+
+module.exports = {
+  getAllLaunches,
+  addNewLaunch,
+};
